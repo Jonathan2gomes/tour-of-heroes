2d5119da338eba2a2cf84fc02322bc43a4139708
jonathan2gomes <jonathan2gomes.jg@gmail.com>
2020-02-27 00:04:51 -0300

    list of heroes working, and select style

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './Hero';
+
+export const  HEROES: Hero[] = [
+  {id: 11, name: 'Superman'},
+  {id: 12, name: 'Cyclops'},
+  {id: 13, name: 'Midnight'},
+  {id: 14, name: 'Wolverine'},
+  {id: 15, name: 'Batman'},
+  {id: 16, name: 'Flash'},
+  {id: 17, name: 'IronMan'},
+  {id: 18, name: 'Wonder Woman'},
+  {id: 19, name: 'Ajax'},
+  {id: 20, name: 'Static Shock'}
+];
