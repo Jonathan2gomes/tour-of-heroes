ef63b873f9d2df5d8411dfd7a0133910683f6c58
jonathan2gomes <jonathan2gomes.jg@gmail.com>
2020-03-01 00:10:16 -0300

    adding in-app navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -9,20 +10,15 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
-constructor(private heroService: HeroService) {
-}
-ngOnInit() {
-  this.getHeroes();
-}
-getHeroes(){
-  this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
-}
+  constructor(private heroService: HeroService) { }
 
-  onSelect(hero: Hero) {
-this.selectedHero = hero;
-console.log(this.selectedHero);
+  ngOnInit() {
+    this.getHeroes();
+  }
 
-}
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 }
